ca77c0d679d0896b7c3078d6f3cbc91d58c0926e
azidaner01 <90376039+azidaner01@users.noreply.github.com>
2021-09-21 15:51:16 +0700

    Add files via upload

---
+++ Tugas2b.py
@@ -0,0 +1,6 @@
+print("MENGHITUNG VOLUME BALOK DENGAN PYTHON")
+p = float(input("\nMasukan Panjang  : "))
+l = float(input("Masukan Lebar: "))
+t = float(input("Masukan Tinggi : "))
+volume = p * l * t
+print ("Volume Balok adalah = ", volume)
